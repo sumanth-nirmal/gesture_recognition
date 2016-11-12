e9b48a54623dad997983b45a6e168492610bcd9c
Akshat <akshatbjain93@gmail.com>
2016-11-12 02:06:02 -0500

    Modified code to load weights saved for epochs 1 to 10

--- gesture_training.py
+++ gesture_training.py
@@ -3,13 +3,13 @@ from __future__ import print_function
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
 from keras.utils import np_utils
 #import dataUtils
 import numpy as np
 
-batch_size = 30
+batch_size = 20
 nClasses = 5
 nEpoch = 60
 dataAugmentation = False
@@ -21,12 +21,12 @@ test_datagen = ImageDataGenerator()
 train_generator = train_datagen.flow_from_directory(
         '/home/akshat/deep_learning/Consolidated_Data_Set/Dataset/training',
         target_size=(224, 224),
-        batch_size=50)
+        batch_size=batch_size)
 
 validation_generator = test_datagen.flow_from_directory(
         '/home/akshat/deep_learning/Consolidated_Data_Set/Dataset/validation',
         target_size=(224, 224),
-        batch_size=50)
+        batch_size=batch_size)
 
 #################################################
 '''
@@ -40,7 +40,7 @@ print(X_test.shape[0], 'test samples')
 Y_train = np_utils.to_categorical(y_train, nClasses)
 Y_test = np_utils.to_categorical(y_test, nClasses)
 '''
-
+'''
 model = Sequential()
 
 model.add(Convolution2D(16, 3, 3, border_mode='same',input_shape=(3,224,224)))
@@ -64,6 +64,53 @@ model.add(Dropout(0.5))
 model.add(Dense(nClasses))
 model.add(Activation('softmax'))
 model.summary()
+'''
+
+model = Sequential()
+model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nClasses, activation='softmax'))
+
+model.load_weights("model_10_epochs.h5")
 
 sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy',
